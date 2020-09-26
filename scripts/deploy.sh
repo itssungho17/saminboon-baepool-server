a7b5a338cfcd5a44f5ed753ec5520cb0647c304f
Sungho <itssungho@naver.com>
2020-09-26 21:14:55 +0900

    Use Travis CI Test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -31,7 +31,7 @@ echo "> Execute $JAR_NAME"
 
 chmod +x $JAR_NAME
 
-nohup java -jar -Dspring.config.location=classpath:/application.properties,\
-classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,\
-/home/ec2-user/app/application-real-db.properties -Dspring.profiles.active=real\
-$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar \
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.profiles.active=real \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &

5091822e4363bdfa7e6d7db3e41039ff05c7fa9c
Ethan <ksr20612@daum.net>
2020-11-19 20:20:39 +0900

    start

---
+++ src/test/java/com/saminboon/baepool/BaepoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saminboon.baepool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaepoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
